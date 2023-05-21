04b54dc5ab1c8d0efef6388e350ef73d4895d067
Joseph Maldjian <josephavm@icloud.com>
2023-05-21 10:09:27 -0500

    Initial Commit

---
+++ Hye Ararat/Hye_AraratApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hye_AraratApp.swift
+//  Hye Ararat
+//
+//  Created by Joseph Maldjian on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Hye_AraratApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
